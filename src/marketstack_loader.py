a935d2198389201a5003463050d14b4d9a84ad95
Lorenzo333c <lcarriero41.lc@gmail.com>
2026-01-26 17:34:33 +0100

    first commit

--- src/marketstack_loader.py
+++ src/marketstack_loader.py
@@ -1,28 +1,32 @@
 import pandas as pd
 import requests
 
-def load_marketstack_data(
-        api_key,
-        symbol,
-        start_date,
-        end_date
-):
+def load_marketstack_data(api_key, symbol, start_date, end_date):
+    import requests
+    import pandas as pd
+
     url = "http://api.marketstack.com/v1/eod"
+
     params = {
-        'access_key': api_key,
-        'symbols': symbol,
-        'date_from': start_date,
-        'date_to': end_date,
-        'limit': 2000
+        "access_key": api_key,
+        "symbols": symbol,
+        "date_from": start_date,
+        "date_to": end_date
     }
 
-    response = requests.get(url, params = params)
+    response = requests.get(url, params=params)
     data = response.json()
 
-    df = pd.DataFrame(data['data'])
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.sort_values('date')
+    #check
+    if "data" not in data:
+        raise ValueError(f"Errore Marketstack per {symbol}: {data}")
+
+    df = pd.DataFrame(data["data"])
+    df["date"] = pd.to_datetime(df["date"])
+    df = df.sort_values("date")
+
     return df
+
 
 def load_multiple_marketstack_data(api_key,symbols,start_date, end_date):
     import pandas as pd
